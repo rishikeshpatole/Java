7fe784a02d36dcf1afdf5f51256b34277860884f
rishikeshpatole <patolerishi108@gmail.com>
2022-11-25 23:35:54 +0530

    Hello world

---
+++ CoreJava/src/javaBasics/L1_HelloWorld.java
@@ -0,0 +1,12 @@
+package javaBasics;
+
+public class L1_HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Welcome to Java basics");
+	}
+
+}
